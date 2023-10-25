940735990409937f6ac527040a2e2c0de956569b
JonesSodaOP <148819585+JonesSodaOP@users.noreply.github.com>
2023-10-25 05:46:25 +0000

    Final code for HW1
    I believe I used the correct functions to accomplish what was necessary

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(){
+  int feet;
+  int inches;
+  int height;
+  
+  cout<< "Please enter your height (feet inches) : ";
+  cin>> feet >> inches; // combined instead of using two cin inputs
+  
+  height = 12 * feet + inches;
+
+  if (height < 60) { // if statement used to give a value for anyone under 5 feet
+      cout << "You must be 5 feet or taller for the calculation." << endl;
+  } else {
+  
+  cout<< "If you are a male, your ideal weight should be "<< 106 + 6 * (height - 60) << " pounds." << endl;
+  cout<< "If you are a female, your ideal weight should be "<< 100 + 5 * (height - 60) << " pounds." << endl;
+  }
+    
+  return 0; 
 }
+// end of code
